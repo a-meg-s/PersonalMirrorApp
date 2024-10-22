9eb7f8dca47c2252d6e778f4cdd97d39d3fd5fce
mariamicioni <maria.micioni@stud.hslu.ch>
2024-10-22 19:43:34 +0200

    delete autogenerated files

--- AppFilesAlenka/UIMirror/opencv/build/intermediates/cxx/Debug/41kn1z11/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\nicog\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\nicog\\AndroidStudioProjects\\IIP-Repository\\spiegelapp-blanktemplate\\AppFilesAlenka\\UIMirror\\opencv\\.cxx\\Debug\\41kn1z11\\x86_64" ^
-  opencv_jni_shared
