7b13389bb92b2206a41c534fc7c742d54f6d2089
Megan <codebymegan@gmail.com>
2024-10-27 13:22:05 +0100

    troubleshooting android studio issues

---
+++ AppFilesAndrea/UIMirror/opencv/build/intermediates/cxx/Debug/73b424g4/logs/arm64-v8a/clean_command.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Andrea\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Andrea\\OneDrive - Hochschule Luzern\\Desktop\\IIP-Mirror\\try2\\AppFilesAndrea\\UIMirror\\opencv\\.cxx\\Debug\\73b424g4\\arm64-v8a" ^
+  clean
