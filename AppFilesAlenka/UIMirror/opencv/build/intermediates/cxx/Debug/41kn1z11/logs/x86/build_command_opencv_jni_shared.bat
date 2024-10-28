d86c10ffed4952c6a9cf327947728a717ed59059
Zinga14 <ale.isenring@gmx.ch>
2024-10-28 08:59:26 +0100

    Test Name at Greeting text added

--- AppFilesAlenka/UIMirror/opencv/build/intermediates/cxx/Debug/41kn1z11/logs/x86/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\nicog\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\nicog\\AndroidStudioProjects\\IIP-Repository\\spiegelapp-blanktemplate\\AppFilesAlenka\\UIMirror\\opencv\\.cxx\\Debug\\41kn1z11\\x86" ^
-  opencv_jni_shared
