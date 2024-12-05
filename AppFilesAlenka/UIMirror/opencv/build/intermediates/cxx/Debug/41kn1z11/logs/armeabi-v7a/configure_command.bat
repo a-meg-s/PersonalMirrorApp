98686cb2f187abbc70eb0078adb0ab5a10e122bd
Nico Graf <nico.graf@stud.hslu.ch>
2024-12-05 11:38:39 +0100

    Pushing newest Version againg, after ensuring functionality

--- AppFilesAlenka/UIMirror/opencv/build/intermediates/cxx/Debug/41kn1z11/logs/armeabi-v7a/configure_command.bat
+++ AppFilesAlenka/UIMirror/opencv/build/intermediates/cxx/Debug/41kn1z11/logs/armeabi-v7a/configure_command.bat
@@ -1,6 +1,6 @@
 @echo off
 "C:\\Users\\nicog\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
-  "-HC:\\Users\\nicog\\AndroidStudioProjects\\IIP-Repository\\spiegelapp-blanktemplate\\AppFilesAlenka\\UIMirror - Kopie\\opencv\\libcxx_helper" ^
+  "-HC:\\Users\\nicog\\AndroidStudioProjects\\IIP-Repository\\spiegelapp-blanktemplate\\AppFilesAlenka\\UIMirror\\opencv\\libcxx_helper" ^
   "-DCMAKE_SYSTEM_NAME=Android" ^
   "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
   "-DCMAKE_SYSTEM_VERSION=21" ^
@@ -11,9 +11,9 @@
   "-DCMAKE_ANDROID_NDK=C:\\Users\\nicog\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125" ^
   "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\nicog\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125\\build\\cmake\\android.toolchain.cmake" ^
   "-DCMAKE_MAKE_PROGRAM=C:\\Users\\nicog\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\nicog\\AndroidStudioProjects\\IIP-Repository\\spiegelapp-blanktemplate\\AppFilesAlenka\\UIMirror - Kopie\\opencv\\build\\intermediates\\cxx\\Debug\\41kn1z11\\obj\\armeabi-v7a" ^
-  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\nicog\\AndroidStudioProjects\\IIP-Repository\\spiegelapp-blanktemplate\\AppFilesAlenka\\UIMirror - Kopie\\opencv\\build\\intermediates\\cxx\\Debug\\41kn1z11\\obj\\armeabi-v7a" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\nicog\\AndroidStudioProjects\\IIP-Repository\\spiegelapp-blanktemplate\\AppFilesAlenka\\UIMirror\\opencv\\build\\intermediates\\cxx\\Debug\\41kn1z11\\obj\\armeabi-v7a" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\nicog\\AndroidStudioProjects\\IIP-Repository\\spiegelapp-blanktemplate\\AppFilesAlenka\\UIMirror\\opencv\\build\\intermediates\\cxx\\Debug\\41kn1z11\\obj\\armeabi-v7a" ^
   "-DCMAKE_BUILD_TYPE=Debug" ^
-  "-BC:\\Users\\nicog\\AndroidStudioProjects\\IIP-Repository\\spiegelapp-blanktemplate\\AppFilesAlenka\\UIMirror - Kopie\\opencv\\.cxx\\Debug\\41kn1z11\\armeabi-v7a" ^
+  "-BC:\\Users\\nicog\\AndroidStudioProjects\\IIP-Repository\\spiegelapp-blanktemplate\\AppFilesAlenka\\UIMirror\\opencv\\.cxx\\Debug\\41kn1z11\\armeabi-v7a" ^
   -GNinja ^
   "-DANDROID_STL=c++_shared"
